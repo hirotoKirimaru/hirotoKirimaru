ff1699374152f7416b77451c3fb66089c12b8e19
kirimaru <nainaistar@gmail.com>
2021-01-09 11:48:40 +0000

    [Add] URLを取得する

--- python/main.py
+++ python/main.py
@@ -2,9 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 # Webページを取得して解析する
 
-load_url = "https://www.ymori.com/books/python2nen/test1.html"
+load_url = "https://lapras.com/public/JFCUKEW"
 html = requests.get(load_url)
 soup = BeautifulSoup(html.content, "html.parser")
 
 # HTML全体を表示する
-print(soup)
+# print(soup)
+content = ""
+for tag in soup.find_all(name = "meta"):
+    content = tag['content']
+    if "https://media.lapras.com/media/public_setting" in content:
+        break
+
+print(content)
